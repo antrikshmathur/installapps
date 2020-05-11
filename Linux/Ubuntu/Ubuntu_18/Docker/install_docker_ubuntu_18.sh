ae979f5ae439017803a552f2c935a61dcf38e13f
triksma <antriksh.mathur@gmail.com>
2020-05-11 17:44:18 +0530

    make docker-compose executable

--- Linux/Ubuntu/Ubuntu_18/Docker/install_docker_ubuntu_18.sh
+++ Linux/Ubuntu/Ubuntu_18/Docker/install_docker_ubuntu_18.sh
@@ -7,3 +7,4 @@ sudo systemctl status docker
 
 #install compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
